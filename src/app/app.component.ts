7134a8ee771f677b74486437920068b2163513c0
Santiago Vallejos <vallejossantiago1412@gmail.com>
2025-01-06 20:17:15 -0300

    CRUD design with modal and bootstrap integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { CrudComponent } from './components/crud/crud.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalendarComponent],
+  imports: [RouterOutlet, CalendarComponent, CrudComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/crud/crud.component.ts
@@ -0,0 +1,12 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  imports: [],
+  templateUrl: './crud.component.html',
+  styleUrl: './crud.component.css'
+})
+export class CrudComponent {
+  
+}
